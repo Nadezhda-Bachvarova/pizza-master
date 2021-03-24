acbfeef8cd53ff85a0dc90471172d74bfc9b52b8
Nadezhda <nadezhda_bachvarova@abv.bg>
2021-03-24 10:09:16 +0200

    update component HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React,{ Component } from 'react';
 import './App.css';
+import HomePage from './components/HomePage/HomePage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  withRouter,
+} from "react-router-dom";
 
 class App extends Component {
   render () {
     return (
-      <div>
-        PIZZA
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+          </Switch>
+        </Router>
       </div>
     )
   }
